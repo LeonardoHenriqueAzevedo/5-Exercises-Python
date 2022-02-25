21cc9365cebc907a549831471f280e3ad3ba522c
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-02-25 20:58:39 -0300

    ex 1

--- script.py
+++ script.py
@@ -1,2 +1,9 @@
 # 1. Escrever um algoritmo que leia um valor inteiro e mostre na tela se é par ou impar.
+print("Descubra agora se o número é par ou ímpar")
 num = float(input("Informe um número: "))
+resto = num % 2
+
+if resto == 0:
+    print("O número {} é um PAR.".format(num))
+else:
+    print("O número {} é ÍMPAR".format(num))
