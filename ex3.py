6e6aca784f86e67d3ee67f117afdfabaff1f1091
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-02-25 22:40:00 -0300

    Update ex3.py

---
+++ ex3.py
@@ -0,0 +1,5 @@
+""" 3. Tendo como dados de entrada a altura (h) e o sexo de uma pessoa, construa um algoritmo que calcule seu
+“peso”(p) ideal, utilizando as seguintes fórmulas:
+Homens: (72.7 * h) – 58
+Mulher: (62.1 * h) – 44.7
+"""
