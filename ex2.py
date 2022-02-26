74487641fafa5ec14842a53183ecfa58d1235921
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-02-25 21:22:28 -0300

    code

--- ex2.py
+++ ex2.py
@@ -1,20 +1,9 @@
 # 2. Escreva um algoritmo que leia três valores inteiros e mostre em ordem decrescente.
 print("Ordem Decrescente dos números")
-n1 = int(input("Informe o primeiro número: "))
-n2 = int(input("Informe o segundo número: "))
-n3 = int(input("Informe o terceiro número: "))
+lista = []
+for n in range(3):
+    num = int(input("Informe um número: "))
+    lista.append(num)
 
-if n1 < n2:
-    if n2 < n3:
-        maior = n3
-    else:
-        maior = n2
-else:
-    if n1 < n3:
-        menor = n1
-        mediano = n3
-    else:
-        mediano = n1
-        menor = n3
-
-print("Ordem Decrescente: {}, {}, {}".format(maior, mediano, menor))
+lista.sort(reverse=True)
+print(lista)
