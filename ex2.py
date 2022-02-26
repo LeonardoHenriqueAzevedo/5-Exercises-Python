afda74f64ebcbdfcf14404a2570914eb225766d5
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-02-25 21:01:37 -0300

    Create ex2.py

---
+++ ex2.py
@@ -0,0 +1,5 @@
+# 2. Escreva um algoritmo que leia três valores inteiros e mostre em ordem decrescente.
+print("Ordem Decrescente dos números")
+n1 = int(input("Informe o primeiro número: "))
+n2 = int(input("Informe o segundo número"))
+n3 = int(input("Informe o terceiro número"))
