96223e08a7e1f3951e58669c67f91ba35789dc46
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-02-25 21:02:16 -0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,16 +1,9 @@
 # 1. Escrever um algoritmo que leia um valor inteiro e mostre na tela se é par ou impar.
-# print("Descubra agora se o número é par ou ímpar")
-# num = float(input("Informe um número: "))
-# resto = num % 2
+print("Descubra agora se o número é par ou ímpar")
+num = float(input("Informe um número: "))
+resto = num % 2
 
-# if resto == 0:
-#     print("O número {} é um PAR.".format(num))
-# else:
-#     print("O número {} é ÍMPAR".format(num))
-
-
-# 2. Escreva um algoritmo que leia três valores inteiros e mostre em ordem decrescente.
-print("Ordem Decrescente dos números")
-n1 = int(input("Informe o primeiro número: "))
-n2 = int(input("Informe o segundo número"))
-n3 = int(input("Informe o terceiro número"))
+if resto == 0:
+    print("O número {} é um PAR.".format(num))
+else:
+    print("O número {} é ÍMPAR".format(num))
