3d23176550eadb95dba44dc0d853519ff27eb255
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-03-08 19:34:47 -0300

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -3,3 +3,14 @@
 Homens: (72.7 * h) – 58
 Mulher: (62.1 * h) – 44.7
 """
+sexo = str(input("Informe seu sexo [M/F]: "))
+altura = float(input("Informe sua altura em metros: "))
+
+if sexo[0].capitalize() == "M":
+    peso_ideal = (72.7 * altura) - 58
+    print(
+        f"O Peso Ideal para uma pessoa do sexo masculino com {altura} de altura deve ser {peso_ideal} kg.")
+else:
+    peso_ideal = (62.1 * altura) - 44.7
+    print(
+        f"O Peso Ideal para uma pessoa do sexo feminino com {altura} de altura deve ser {peso_ideal} kg.")
