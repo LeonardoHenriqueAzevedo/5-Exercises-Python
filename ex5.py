89255c44c40ca6675d9345feaa7269555bbe6f6b
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-03-08 20:58:13 -0300

    Update ex5.py

---
+++ ex5.py
@@ -0,0 +1,13 @@
+"""5. Escreva um algoritmo que leia o código de um determinado
+produto e mostre a sua classificação utilize a seguinte tabela
+como referência. (escolha... caso)
+Codigo - Classificação
+1 - Alimento não – perecível
+2,3 ou 4 - Alimento perecível
+5 ou 6 - Vestuário
+7 - Higiene pessoal
+8 até 15 - Limpeza e utensílios domésticos
+Qualquer outro código inválido
+"""
+
+cod = int(input("Informe "))
