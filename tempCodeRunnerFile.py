37abeae8bb58a5349d4635eca4dc206e457876ef
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-03-08 21:29:23 -0300

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,4 @@
-print(lista)
+ Faça um algoritmo que leia o ano de nascimento de uma
+pessoa, calcule e mostre a sua idade e, também, verifique e
+mostre se ela já tem idade para votar (16 anos ou mais) e para
+conseguir a carteira de habilitação (18 anos ou mais)."
