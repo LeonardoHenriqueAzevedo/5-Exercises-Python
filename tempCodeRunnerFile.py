01e603322834d151c6be74105450419e4b5a8755
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-03-08 21:32:28 -0300

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
- Faça um algoritmo que leia o ano de nascimento de uma
-pessoa, calcule e mostre a sua idade e, também, verifique e
-mostre se ela já tem idade para votar (16 anos ou mais) e para
-conseguir a carteira de habilitação (18 anos ou mais)."
